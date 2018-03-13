f73047d96882900e58f445bcfa7b786acd92e50c
Thomas Castleman <thomas7rex@gmail.com>
2018-03-13 09:01:48 -0400

    Added deps in server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+
+var express         = require('express');
+var mustacheExpress = require('mustache-express');
+var bodyParser      = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.engine('html', mustacheExpress());
+app.use('/', express.static('static'));
 var cal = require('ical');
+
+
 cal.fromURL('https://api.veracross.com/stab/subscribe/14F14099-B662-439F-BBCE-9CF991E8DA96.ics',{}, function(err,data){
 	//console.log(data);
 	//console.log(err);
