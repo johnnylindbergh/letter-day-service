db1d371faba10ea2b988d86fc110230c5a114901
Thomas Castleman <thomas7rex@gmail.com>
2018-03-13 14:21:31 -0400

    Split into files

---
+++ letterday.js
@@ -0,0 +1,31 @@
+var moment = require('moment.js');
+var private = require('private.js');
+
+module.exports = {
+	// make api call and update last modified date
+	updateLetterDay: function(callback) {
+		lastUpdate = moment();	// update last update
+		currentLetterDay = undefined;
+
+		cal.fromURL(private.lindbergh_school_events,{}, function(err,data) {
+			if (err) throw err;
+
+			for (var k in data){
+				if (data.hasOwnProperty(k)) {
+					var ev = data[k]
+					var summary = ev.summary;
+					if (summary.match(/(D|d)ay\s\w\s\(US\)/g)) {
+						var event_date = moment(ev.start);
+
+						if (event_date.isSame(lastUpdate, 'day')) {
+							currentLetterDay = summary.split(' ')[1];
+							break;
+						}
+					}
+				}
+			}
+
+			callback();
+		});
+	}
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser      = require('body-parser');
 var moment			= require('moment');
 var cal 			= require('ical');
 var private			= require('./private.js');
+var letterDay		= require('./letterday.js');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,48 +15,10 @@ app.use('/', express.static('static'));
 var lastUpdate;
 var currentLetterDay;
 
-//debug
-updateLetterDay(function() {
-	console.log("Finished,.");
-});
-
-// console.log("Event",
-// 	ev.summary,
-// 	'is in',
-// 	ev.location,
-// 	'on', ev.start.getDate());
-
-// make api call and update last modified date
-function updateLetterDay(callback) {
-	lastUpdate = moment();	// update last update
-	currentLetterDay = undefined;
-
-	cal.fromURL(private.lindbergh_school_events,{}, function(err,data) {
-		if (err) throw err;
-
-		for (var k in data){
-			if (data.hasOwnProperty(k)) {
-				var ev = data[k]
-				var summary = ev.summary;
-				if (summary.match(/(D|d)ay\s\w\s\(US\)/g)) {
-					var event_date = moment(ev.start);
-
-					if (event_date.isSame(lastUpdate, 'day')) {
-						currentLetterDay = summary.split(' ')[1];
-						break;
-					}
-				}
-			}
-		}
-
-		callback();
-	});
-}
-
 app.get('/', function(req, res) {
 	var today = moment();
 	if (!today.isSame(lastUpdate, 'day')) {
-		updateLetterDay(function() {
+		letterDay.updateLetterDay(function() {
 			res.send(currentLetterDay);
 		});
 	} else {
