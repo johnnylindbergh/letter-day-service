85206c8727ffe2e9fb624acf47e1877e1b7a83a2
Thomas Castleman <thomas7rex@gmail.com>
2018-03-13 09:03:39 -0400

    err message

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use('/', express.static('static'));
 
 
 cal.fromURL('https://api.veracross.com/stab/subscribe/14F14099-B662-439F-BBCE-9CF991E8DA96.ics',{}, function(err,data){
-	//console.log(data);
-	//console.log(err);
+	if (err) throw err;
+	
 	for (var k in data){
 		if (data.hasOwnProperty(k)) {
 			var ev = data[k]
